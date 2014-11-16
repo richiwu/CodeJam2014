5e56b1208f6bcda116684b662267333d95e52eb9
Eddy Lu <multiworldhome@yahoo.com>
2014-11-16 12:45:03 -0500

    modify imageToArray.java

--- imageToArray.java
+++ imageToArray.java
@@ -2,7 +2,6 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-
 public class imageToArray {
 	
 	private static int height;
@@ -10,30 +9,32 @@ public class imageToArray {
 	private static int[][] outputArray;
 	
 	public static void main(String[] args) throws IOException {
+		
 		String inputFileName = "1_2_.gif";
 		File inputFile = new File(inputFileName);
-		outputArray = convertImageToArray(inputFile);
-		
+		imageToArray imageToArray = new imageToArray();
+		outputArray = imageToArray.convertImageToArray(inputFile);
 		for (int i=0; i<height; i++) {
 			for (int j=0; j<width; j++) {
 				System.out.print(outputArray[i][j]);
 			}
 			System.out.println();
 		}
+		
 	}
 	
-	public static int[][] convertImageToArray(File inputFile) throws IOException {
+	public int[][] convertImageToArray(File inputFile) throws IOException {
+		
 		BufferedImage image = ImageIO.read(inputFile);
 		width = image.getWidth();
 		height = image.getHeight();
-	    int[][] result = new int[height][width];
-	    
-	    for (int i=0; i<height; i++) {
-	    	for (int j=0; j<width; j++) {
-	    		result[i][j] = image.getRGB(j, i);
-	    	}
-	    }
-	    return result;
-    }
+		int[][] result = new int[height][width];
+		for (int i=0; i<height; i++) {
+			for (int j=0; j<width; j++) {
+				result[i][j] = image.getRGB(j, i);
+			}
+		}
+		return result;
+	}
 	
 }
